f2e8c3a51c0c26e50a28192be2fe0369f948b635
aqeeluser404 <aqeelhanslo@gmail.com>
2022-10-11 13:03:23 +0200

    Made skeleton BookGUI Made class to code our main page. Goodluck bro

---
+++ src/main/java/za/ac/mycput/libraryclient/views/HomeGui.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package za.ac.mycput.libraryclient.views;
+
+/**
+ *
+ * @author 219374422
+ */
+public class HomeGui {
+    
+}
